42f87c99ca3ddb17f2fcf2c45e71941d4d7c8e11
João Matheus <138176279+joaomatheusm@users.noreply.github.com>
2023-11-15 18:51:08 -0300

    v1.0

---
+++ Cachorro.py
@@ -0,0 +1,80 @@
+from random import randint
+
+class Cachorro:
+    def __init__(self, nome, raca, sexo, idade):
+        self.nome = nome
+        self.raca = raca
+        sexos = ['M', 'm', 'F', 'f']
+        if sexo in sexos:
+            self.sexo = sexo
+        else:
+            print('Sexo inválido.')
+            self.sexo = 'N'
+        self.idade = idade
+        self.energia = 100
+        self.numero_filhotes = 0
+
+
+    def obter_dados(self):
+        return (f'Nome: {self.nome}\n'
+                f'Raça: {self.raca}\n'
+                f'Sexo: {self.sexo}\n'
+                f'Idade: {self.idade}\n'
+                f'Energia: {self.energia}\n'
+                f'Número de filhotes: {self.numero_filhotes}')
+
+
+    def comer(self, comida):
+        if self.energia <= 50:
+            print('R) Ração\n'
+                  'C) Carne\n'
+                  'L) Legumes')
+            while True:
+                comida = str(input('Escolha uma opção: ')).upper().strip()[0]
+                if comida in 'RCL':
+                    break
+                else:
+                    print('Opção inválida!', end=' ')
+            if comida == 'R':
+                self.energia += 50
+            elif comida == 'C':
+                self.energia += 40
+            elif comida == 'L':
+                self.energia += 30
+            print(f'O cachorro foi alimentado!', end=' ')
+        else:
+            print('O cachorro não precisa comer.', end=' ')
+        print(f'Energia atual: {self.energia}')
+
+
+    def brincar(self, brincadeira):
+        if self.energia >= 40:
+            if brincadeira == 'B':
+                self.energia -= 30
+            elif brincadeira == 'S':
+                self.energia -= 20
+            elif brincadeira == 'G':
+                self.energia -= 10
+            print(f'O cachorro brincou de {brincadeira}.', end=' ')
+        else:
+            print('O cachorro não pode brincar agora.', end=' ')
+        print(f'Energia atual: {self.energia}')
+
+
+    def pode_cruzar(self, parc):
+        if 1 <= self.idade <= 9 and 1 <= parc.idade <= 9 and self.raca == parc.raca and self.energia >= 80 and parc.energia >= 80 and self.sexo != parc.sexo:
+            return True
+        else:
+            return False
+
+
+    def cruzar(self, parc):
+        if self.pode_cruzar(parc):
+            self.energia -= 50
+            parc.energia -= 50
+            filhotes_gerados = randint(3, 10)
+            self.numero_filhotes += filhotes_gerados
+            parc.numero_filhotes += filhotes_gerados
+            print(f'Cruzamento realizado. Filhotes gerados: {filhotes_gerados}')
+        else:
+            print('Não foi possivel realizar o cruzamento.')
---
+++ CachorroMain.py
@@ -0,0 +1,99 @@
+from Cachorro import Cachorro
+from sys import exit
+
+
+def manipular_cachorro(cao_atual, lista_caes):
+    try:
+        while True:
+            print('-' * 30)
+            print('1) Ver dados do cão\n'
+                  '2) Alimentar\n'
+                  '3) Brincar\n'
+                  '4) Cruzar\n'
+                  '5) Menu inicial')
+            opcao = int(input('Escolha uma opção: '))
+            print('-' * 30)
+
+            if opcao == 1:
+                print(cao_atual.obter_dados())
+            elif opcao == 2:
+                cao_atual.comer('comida')
+            elif opcao == 3:
+                if cao_atual.energia >= 40:
+                    print('B) Buscar bolinha\n'
+                          'S) Saltar\n'
+                          'G) Girar')
+                    while True:
+                        brincadeira = str(input('Escolha uma opção: ')).upper().strip()[0]
+                        if brincadeira in 'BSG':
+                            break
+                        else:
+                            print('Opção inválida!', end=' ')
+                    cao_atual.brincar(brincadeira)
+                else:
+                    print('O cachorro não pode brincar agora.')
+            elif opcao == 4:
+                print(f'{" COD":<4}{"NOME":>20}')
+                for c in lista_caes:
+                    if cao_atual.pode_cruzar(c):
+                        print(f'  {lista_caes.index(c):<4}{c.nome:>20}')
+                while True:
+                    parceiro = int(input('Escolha um parceiro para o cachorro cruzar: '))
+                    if cao_atual.pode_cruzar(lista_caes[parceiro]):
+                        break
+                    else:
+                        print('Opção indisponível.', end=' ')
+                cao_atual.cruzar(lista_caes[parceiro])
+            elif opcao == 5:
+                return
+            else:
+                print('Opção inválida.')
+    except ValueError:
+        print('Opção inválida.')
+
+
+def main():
+    lista_caes = []
+    while True:
+        try:
+            print('-' * 30)
+            print('1) Cadastrar cão\n'
+                  '2) Listar cães\n'
+                  '3) Sair do programa')
+            opcao = int(input('Escolha uma opção: '))
+            print('-' * 30)
+
+            if opcao == 1:
+                nome = str(input('Nome: '))
+                raca = str(input('Raça: '))
+                while True:
+                    sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
+                    if sexo in 'MF':
+                        break
+                    else:
+                        print('Opção inválida.', end=' ')
+                while True:
+                    try:
+                        idade = int(input('Idade: '))
+                    except ValueError:
+                        print('Opção inválida.', end=' ')
+                    else:
+                        break
+
+                lista_caes.append(Cachorro(nome, raca, sexo, idade))
+            elif opcao == 2:
+                print(f'{" COD":<4}{"NOME":>20}')
+                for i, c in enumerate(lista_caes):
+                    print(f'  {i:<1}{c.nome:>20}')
+                cao_atual = int(input('Escolha um cão para manipular: '))
+                manipular_cachorro(lista_caes[cao_atual], lista_caes)
+            elif opcao == 3:
+                print('<< PROGRAMA ENCERRADO >>')
+                exit(0)
+            else:
+                print('Opção inválida.')
+        except ValueError:
+            print('Opção inválida')
+
+
+main()
